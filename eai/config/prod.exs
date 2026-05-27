9e65920e7e8d25f3edcacad7b222f85475163dba
TurinFohlen <postdimension@163.com>
2026-05-27 06:13:26 +0000

    提供一键安装脚本，丰富了可配置项

--- eai/config/prod.exs
+++
@@ -1,3 +0,0 @@
-import Config
-
-# 生产环境：所有敏感配置通过环境变量注入，见 runtime.exs
--- eai/lib/eai/cache/cache.ex
+++
@@ -1,7 +0,0 @@
-defmodule Eai.Cache.Cache do
-  @moduledoc "Nebulex cache adapter for EAI"
-
-  use Nebulex.Cache,
-    otp_app: :eai,
-    adapter: Nebulex.Adapters.Local
-end
