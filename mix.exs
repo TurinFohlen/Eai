ae244487b2bbb283ea25eb72cfe5dcaff9fe6d8a
TurinFohlen <postdimension@163.com>
2026-06-22 19:34:30 +0000

    大幅完善系统预制角色卡功能

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Eai.MixProject do
   def project do
     [
       app: :eai,
-      version: "1.0.3",
+      version: "1.0.4",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
